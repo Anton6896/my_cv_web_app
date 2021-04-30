c4d1256704180b61f08f39f3a2b81ba0a5946a79
AntLinux <anton-r@live.com>
2021-04-30 10:39:45 +0300

    register user page

--- core_cv/users/views.py
+++ core_cv/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from .models import Profile
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 
 User = settings.AUTH_USER_MODEL
 
@@ -20,6 +21,27 @@ class HomeUsers(View):
         }
 
         return render(self.request, "home_users.html", context)
+
+    def post(self, *args, **kwarg):
+        pass
+
+
+class Register(View):
+    # user creation form (django build in)
+
+    def __init__(self):
+        super(Register, self).__init__()
+        self.form = None
+
+    def get(self, *args, **kwarg):
+        form = UserCreationForm()
+
+        context = {
+            "title": 'Register',
+            "form": self.form,
+        }
+
+        return render(self.request, "register_users.html", context)
 
     def post(self, *args, **kwarg):
         pass
